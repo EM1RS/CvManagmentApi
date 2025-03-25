1faf3988eddb1eeb6a7f1796b3e2c61a2869761e
EM1RS <emirsokoli@gmail.com>
2025-03-25 20:38:57 +0100

    ReadMedeleted

--- CvManagementApi/obj/Debug/net8.0/CvManagementApi.AssemblyInfo.cs
+++ CvManagementApi/obj/Debug/net8.0/CvManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CvManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10af4fe722f46c7cf59749d6f16d009654d3e07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4628ffb07cf5d213eada9704788731f82b2d16")]
 [assembly: System.Reflection.AssemblyProductAttribute("CvManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CvManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
